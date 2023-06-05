be5db8d8bc47a800c7210d5449a17a23d3099e28
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-05 15:34:46 -0400

    grouped query functions for accessibility

---
+++ db/queries/queries.js
@@ -0,0 +1,12 @@
+
+const chapters = require('./chapters/chapters');
+const users = require('./users/users');
+const stories = require('./stories/stories');
+const votes = require('./votes/votes');
+
+module.exports = {
+  users,
+  chapters,
+  stories,
+  votes,
+}
