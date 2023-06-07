890ab2f11f5b21d00cec8c7d33469ee3a406ed3e
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-07 15:53:15 -0400

    updated stories seed

--- db/seeds/03_stories.sql
+++ db/seeds/03_stories.sql
@@ -1,4 +1,4 @@
 -- Insert seed data into stories table
-INSERT INTO stories (id, title, first_chapter_id, last_chapter_id, complete) VALUES
+INSERT INTO stories (title, first_chapter_id, last_chapter_id, complete) VALUES
                                                                                (1, 'Story 1', 1, 3, FALSE),
                                                                                (2, 'Story 2', 2, 2, TRUE);
