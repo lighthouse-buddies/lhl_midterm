58eada90e3d9c1252c322b4bfc0b96f4cf5bddca
Tamara Tell <2telltamara@gmail.com>
2023-06-07 12:01:58 -0400

    add html for my_stories.ejsand initial css styling to my-stories.scss

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 
-//dummy route to style stories_show template (will eventually become /stories:chapterid)
+//dummy routes to style 
 app.get('/stories', (req, res) => {
   res.render('stories_show');
 });
@@ -61,6 +61,12 @@ app.get('/create', (req, res) => {
 app.get('/login', (req, res) => {
   res.render('login');
 });
+
+app.get('/mystories', (req, res) => {
+  res.render('my_stories');
+});
+
+//end of dummy routes
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
