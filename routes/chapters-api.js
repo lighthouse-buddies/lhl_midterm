522167f0a0f708b7f2b20018900d52afcfb31bd5
Tamara Tell <2telltamara@gmail.com>
2023-06-08 08:48:39 -0400

    add stories_contribute template and route handler to GET it

--- routes/chapters-api.js
+++ routes/chapters-api.js
@@ -1,32 +1,42 @@
 const express = require('express');
-const router  = express.Router();
+const res = require('express/lib/response');
+const router = express.Router();
 const chapterQueries = require('../db/queries/queries');
 
-// POST route to create a new chapter
+// POST route to create a new chapter, save the chapter to the databse. 
+// this post route needs to get the content and prev from the req.body, so either the url path needs to be:
+//<a href="/new?prevChapterId=123">Go to Form</a>
+//or we need a session cookie to tell the chapter
 router.post('/new', (req, res) => {
   const { content, prev } = req.body;
   const email = req.session.email;
   const password = req.body.password;
 
   chapterQueries.chapters.create(content, prev, email, password)
-  .then((chapterId) => {
-    if (chapterId !== null) {
-      return chapterQueries.chapters.getById(chapterId);
-    } else {
+    .then((chapterId) => {
+      if (chapterId !== null) {
+        return chapterQueries.chapters.getById(chapterId);
+      } else {
+        res.status(500).send('Chapter creation failed');
+      }
+    })
+    .then((chapter) => {
+      if (chapter !== null) {
+        res.redirect(`/${chapter.id}`);
+      } else {
+        res.status(500).send('Chapter retrieval failed');
+      }
+    })
+    .catch((error) => {
+      console.log(error);
       res.status(500).send('Chapter creation failed');
-    }
-  })
-  .then((chapter) => {
-    if (chapter !== null) {
-      res.redirect(`/${chapter.id}`);
-    } else {
-      res.status(500).send('Chapter retrieval failed');
-    }
-  })
-  .catch((error) => {
-    console.log(error);
-    res.status(500).send('Chapter creation failed');
-  });
+    });
+});
+
+//GET route to get the chapter contribution form from the previous chapter ID. 
+router.get('/new', (req, res) => {
+  const prevChapterId = req.query.prevChapterId;
+  res.render();
 });
 
 // GET route to get chapter by id
