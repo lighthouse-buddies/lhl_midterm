cc79c6e51e44e0948d0ef820e35cf1c94735c03f
Tamara Tell <2telltamara@gmail.com>
2023-06-08 09:15:55 -0400

    add queries.users.authenticate function to router.post('new') in order to get user_id when creating chapter

--- routes/chapters-api.js
+++ routes/chapters-api.js
@@ -4,36 +4,49 @@ const router = express.Router();
 const chapterQueries = require('../db/queries/queries');
 
 // POST route to create a new chapter, save the chapter to the databse. 
-// this post route needs to get the content and prev from the req.body, so either the url path needs to be:
-//<a href="/new?prevChapterId=123">Go to Form</a>
-//or we need a session cookie to tell the chapter
+// this post route will be getting prev from a hidden input in the form body. Which is linked to on contribute link on the stories_show template. 
+//<a href="/new?prevChapterId=chapID">Contribute</a>
 router.post('/new', (req, res) => {
   const { content, prev } = req.body;
   const email = req.session.email;
-  const password = req.body.password;
+  const password = req.body.password; //need to change to session so we can authenticate the user that way!!!
 
-  chapterQueries.chapters.create(content, prev, email, password)
-    .then((chapterId) => {
-      if (chapterId !== null) {
-        return chapterQueries.chapters.getById(chapterId);
+  // Call the authenticate function to retrieve the user ID
+  chapterQueries.users.authenticate(email, password)
+    .then((userId) => {
+      if (userId !== null) {
+        // Use the retrieved user ID for further processing
+        chapterQueries.chapters.create(content, prev, userId)
+          .then((chapterId) => {
+            if (chapterId !== null) {
+              return chapterQueries.chapters.getById(chapterId);
+            } else {
+              res.status(500).send('Chapter creation failed');
+            }
+          })
+          .then((chapter) => {
+            if (chapter !== null) {
+              res.redirect(`/${chapter.id}`);
+            } else {
+              res.status(500).send('Chapter retrieval failed');
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            res.status(500).send('Chapter creation failed');
+          });
       } else {
-        res.status(500).send('Chapter creation failed');
-      }
-    })
-    .then((chapter) => {
-      if (chapter !== null) {
-        res.redirect(`/${chapter.id}`);
-      } else {
-        res.status(500).send('Chapter retrieval failed');
+        res.status(401).send('Invalid credentials'); // Authentication failed
       }
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).send('Chapter creation failed');
+      res.status(500).send('Error authenticating user');
     });
-});
+});;
 
-//GET route to get the chapter contribution form from the previous chapter ID. 
+//GET route to get the chapter contribution form from the previous chapter ID.
+//this route is redirected to from the 'contribute' link on the stories_show template, which has the current chap ID passed in as a prevChapterId parameter. 
 router.get('/new', (req, res) => {
   const prevChapterId = req.query.prevChapterId;
   res.render('stories_contribute', { prevChapterId });
