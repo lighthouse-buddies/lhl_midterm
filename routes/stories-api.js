3384a9fb302b6f58bbc6e28bb98e87830626eeff
Tamara Tell <2telltamara@gmail.com>
2023-06-08 21:32:29 -0400

    add promise chain to get user information for each chapter to get(/recent/json)

--- routes/stories-api.js
+++ routes/stories-api.js
@@ -95,28 +95,36 @@ router.get('/:id', (req, res) => {
 
 //WORKING! renders json object properly.
 //GET recent stories to render from front-end to homepage
-//This route should return a JSON object containing the 15 most recent stories' information, and corresponding chapter information, which can then be accessed by the front end.
+//This route should return a JSON object containing the 15 most recent stories' information, and corresponding chapter information, as well as the username for each first chapter which can then be accessed by the front end.
 router.get('/recent/json', (req, res) => {
-
   queries.stories.recent(15)
     .then(recentStoryIds => {
-      const recentStoryPromises = recentStoryIds.map((storyId) => queries.stories.getData(storyId));
+      const recentStoryPromises = recentStoryIds.map(storyId => queries.stories.getData(storyId));
       Promise.all(recentStoryPromises)
         .then(recentStories => {
-          const firstChaptersPromise = recentStories.map((story) => queries.chapters.getById(story.first_chapter_id));
+          const firstChaptersPromise = recentStories.map(story => {
+            return queries.chapters.getById(story.first_chapter_id)
+              .then(firstChapter => {
+                return queries.users.get(firstChapter.user_id)
+                  .then(user => {
+                    firstChapter.username = user ? user.username : null;
+                    return firstChapter;
+                  });
+              });
+          });
           Promise.all(firstChaptersPromise)
             .then(firstChapters => {
               const storyData = compileFirstStoryData(recentStories, firstChapters);
-
               res.json({ storyData });
             });
         });
-
-    }).catch((error) => {
+    })
+    .catch(error => {
       console.log(error);
       res.status(500).send('Error: Could not retrieve stories');
     });
 });
+
 
 
 // GET completed stories
