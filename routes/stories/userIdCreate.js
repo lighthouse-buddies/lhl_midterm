e56a968642accc0afe8ab9bf656b6e057a2fd8ef
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-09 08:50:43 -0400

    update create story handler

--- routes/stories/userIdCreate.js
+++ routes/stories/userIdCreate.js
@@ -4,21 +4,25 @@ const queries = require('../../db/queries/queries');
 
 // POST route for user to create a story
 const userCreateStoryHandler = (req, res) => {
-  const { title, chapterId } = req.body;
+  const { title, chapter_content } = req.body;
   const sessionUserId = req.session.userId;
-
+  console.log(req.body);
   if (!sessionUserId) {
     res.status(401).send('Unauthorized');
     return;
   }
 
-  queries.stories.create(title, chapterId,)
-    .then((success) => {
-      if (!success) {
-        throw new Error('Error creating story');
-      }
-      res.redirect(`/stories/${sessionUserId}`);
-    })
+  queries.chapters.create(chapter_content, null, sessionUserId).then(r => {
+  console.log(r);
+
+    queries.stories.create(title, r.id)
+      .then((success) => {
+        if (!success) {
+          throw new Error('Error creating story');
+        }
+        res.redirect(`/stories/${sessionUserId}`);
+      })
+  })
     .catch((error) => {
       console.log(error);
       res.status(500).send('Error creating story');
