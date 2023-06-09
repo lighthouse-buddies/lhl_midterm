3ed7c5c775cae46f4d445df4f5e64c1cd6fb3787
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-08 21:25:42 -0400

    db intialization files updated

---
+++ db/schema/schema.sql
@@ -0,0 +1,6 @@
+\i schema/00_grant.sql
+\i schema/01_users.sql
+\i schema/02_chapters.sql
+\i schema/03_stories.sql
+\i schema/04_votes.sql
+\i schema/05_widgets.sql
---
+++ db/seeds/seeds.sql
@@ -0,0 +1,4 @@
+\i seeds/01_users.sql
+\i seeds/02_chapters.sql
+\i seeds/03_stories.sql
+\i seeds/04_votes.sql
