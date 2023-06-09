8f8286b33e8c0b067a2d74497420ac124c68e3e5
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-09 05:49:12 -0400

    updated chapters:id

--- routes/chapters/idGet.js
+++ routes/chapters/idGet.js
@@ -1,42 +1,61 @@
 const queries = require("../../db/queries/queries");
 
-//NOT WORKING because of queries.chapters.getById. Everything else working fine. Issue with db function
-// GET Route handler for rendering HTML template stories_show using the chapter data. Also renders the username and storytitle as template vars.
+// GET Route handler for rendering HTML template stories_show using the chapter data. Also renders the username, story title, and current chapter number as template vars.
 const chaptersIdGetHandler = (req, res) => {
   const chapterId = req.params.id;
 
+  // Retrieve the chapter data from the database using queries.chapters.getData function
   queries.chapters.getData(chapterId)
-    .then((chapter) => {
-      if (chapter !== null) {
-        const userId = chapter.user_id;
-        const currentChapterNumber = chapter.prev + 1;
+    .then(chapter => {
+      // Retrieve the story ID associated with the chapter using queries.stories.storyOfChapter function
+      queries.stories.storyOfChapter(chapterId)
+        .then(storyId => {
+          // Retrieve the title of the story using queries.stories.getData function
+          queries.stories.getData(storyId)
+            .then(story => {
+              // Retrieve the user ID from the chapter data
+              const userId = chapter.user_id;
 
-        const usernamePromise = queries.users.getData(userId).then((user) => user.username);
-        const storyIdPromise = queries.stories.storyOfChapter(chapterId).then((story) => story.story_id);
-        const storyTitlePromise = queries.stories.getData(storyIdPromise).then((story) => story.title);
+              // Retrieve the user's name using queries.users.getData function
+              queries.users.getData(userId)
+                .then(user => {
+                  // Assuming you have the currentChapterNumber available
+                  const currentChapterNumber = 1;
 
-        Promise.all([usernamePromise, storyTitlePromise])
-          .then(([username, storyTitle, chapterCount]) => {
-            const templateVars = {
-              chapter,
-              username,
-              storyTitle,
-              currentChapterNumber
-            };
-            res.render('stories_show', templateVars);
-          })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error retrieving data');
-          });
-      } else {
-        res.status(404).send('Chapter not found');
-      }
+                  // Prepare the template variables
+                  const templateVars = {
+                    chapter,
+                    username: user.name,
+                    storyTitle: story.title,
+                    currentChapterNumber,
+                    storyId,
+                    //IMPLEMENT
+                    stories : [],
+                    userCookie: req.session.userId
+                  };
+
+                  // Render the 'stories_show' template and pass the template variables to the response
+                  res.render('stories_show', templateVars);
+                })
+                .catch(error => {
+                  console.error('Error retrieving user data:', error);
+                  // Handle the error as needed
+                });
+            })
+            .catch(error => {
+              console.error('Error retrieving story data:', error);
+              // Handle the error as needed
+            });
+        })
+        .catch(error => {
+          console.error('Error retrieving story ID:', error);
+          // Handle the error as needed
+        });
     })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error retrieving chapter');
+    .catch(error => {
+      console.error('Error retrieving chapter:', error);
+      // Handle the error as needed
     });
-}
+};
 
 module.exports = chaptersIdGetHandler;
