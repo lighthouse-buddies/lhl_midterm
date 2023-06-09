8f6cc104a923636cdee81a172ff0ec8c5601b9ca
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-09 08:05:26 -0400

    update new Chapter Post handler

--- routes/chapters/newPost.js
+++ routes/chapters/newPost.js
@@ -5,43 +5,24 @@ const queries = require("../../db/queries/queries");
 // this post route will be getting prev from a hidden input in the form body. Which is linked to on contribute link on the stories_show template.
 //<a href="/new?prevChapterId=chapID">Contribute</a>
 const chaptersNewPostHandler = (req, res) => {
-  const content = req.body;
+  const {prevChapterId, chapter_content} = req.body;
   const prev = req.query.prevChapterId;
-  const email = req.session.email;
-  const password = req.body.password; //need to change to session so we can authenticate the user that way!!!
 
-  // Call the authenticate function to retrieve the user ID
-  queries.users.authenticate(email, password)
-    .then((userId) => {
-      if (userId !== null) {
-        // Use the retrieved user ID for further processing
-        queries.chapters.create(content, prev, userId)
-          .then((chapterId) => {
-            if (chapterId !== null) {
-              return queries.chapters.getData(chapterId);
-            } else {
-              res.status(500).send('Chapter creation failed');
-            }
-          })
-          .then((chapter) => {
-            if (chapter !== null) {
-              res.redirect(`/${chapter.id}`);
-            } else {
-              res.status(500).send('Chapter retrieval failed');
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-            res.status(500).send('Chapter creation failed');
-          });
-      } else {
-        res.status(401).send('Invalid credentials'); // Authentication failed
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500).send('Error authenticating user');
-    });
-}
+  const userId = req.session.userId;
+  if (userId !== null) {
+    // Use the retrieved user ID for further processing
+    queries.chapters.create(chapter_content, prevChapterId, userId)
+      .then((chapterId) => {
+        console.log(chapterId);
+
+        res.redirect(`/chapters/`+chapterId.id);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send('Chapter creation failed');
+      });
+
+  }
+};
 
 module.exports = chaptersNewPostHandler;
