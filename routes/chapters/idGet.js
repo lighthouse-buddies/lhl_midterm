7a7336f28530fe65dba8f3f67f3b2d69dc0d3f57
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-09 06:20:07 -0400

    used promise.all

--- routes/chapters/idGet.js
+++ routes/chapters/idGet.js
@@ -1,59 +1,46 @@
 const queries = require("../../db/queries/queries");
 
-// GET Route handler for rendering HTML template stories_show using the chapter data. Also renders the username, story title, and current chapter number as template vars.
+// GET Route handler for rendering HTML template stories_show using the chapter data. Also renders the username, story title, and chapter number as template vars.
 const chaptersIdGetHandler = (req, res) => {
   const chapterId = req.params.id;
 
-  // Retrieve the chapter data from the database using queries.chapters.getData function
-  queries.chapters.getData(chapterId)
-    .then(chapter => {
-      // Retrieve the story ID associated with the chapter using queries.stories.storyOfChapter function
-      queries.stories.storyOfChapter(chapterId)
-        .then(storyId => {
-          // Retrieve the title of the story using queries.stories.getData function
-          queries.stories.getData(storyId)
-            .then(story => {
-              // Retrieve the user ID from the chapter data
-              const userId = chapter.user_id;
+  // Retrieve the chapter data, story ID, and votes using Promise.all
+  Promise.all([
+    queries.chapters.getData(chapterId),
+    queries.stories.storyOfChapter(chapterId),
+    queries.votes.getChapter(chapterId),
+    queries.chapters.getChapterNumber(chapterId)
+  ])
+    .then(([chapter, storyId, votes, chapterNumber]) => {
+      // Retrieve the user and story data
+      Promise.all([
+        queries.users.getData(chapter.user_id),
+        queries.stories.getData(storyId)
+      ])
+        .then(([user, story]) => {
+          // Prepare the template variables
+          const templateVars = {
+            chapter,
+            complete: story.complete,
+            username: user.name,
+            storyTitle: story.title,
+            currentChapterNumber: chapterNumber,
+            storyId,
+            votes,
+            stories: [],
+            userCookie: req.session.userId
+          };
 
-              // Retrieve the user's name using queries.users.getData function
-              queries.users.getData(userId)
-                .then(user => {
-                  // Assuming you have the currentChapterNumber available
-                  const currentChapterNumber = 1;
-
-                  // Prepare the template variables
-                  const templateVars = {
-                    chapter,
-                    username: user.name,
-                    storyTitle: story.title,
-                    currentChapterNumber,
-                    storyId,
-                    //IMPLEMENT
-                    stories : [],
-                    userCookie: req.session.userId
-                  };
-
-                  // Render the 'stories_show' template and pass the template variables to the response
-                  res.render('stories_show', templateVars);
-                })
-                .catch(error => {
-                  console.error('Error retrieving user data:', error);
-                  // Handle the error as needed
-                });
-            })
-            .catch(error => {
-              console.error('Error retrieving story data:', error);
-              // Handle the error as needed
-            });
+          // Render the 'stories_show' template and pass the template variables to the response
+          res.render('stories_show', templateVars);
         })
         .catch(error => {
-          console.error('Error retrieving story ID:', error);
+          console.error('Error retrieving user or story data:', error);
           // Handle the error as needed
         });
     })
     .catch(error => {
-      console.error('Error retrieving chapter:', error);
+      console.error('Error retrieving data:', error);
       // Handle the error as needed
     });
 };
