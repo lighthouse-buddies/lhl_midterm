c3db05c5353e132437145aab4883b4c61c35e3b5
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-10 14:42:41 -0400

    updated db schema for easier reset

---
+++ db/schema/00_drop.sql
@@ -0,0 +1,11 @@
+-- Drop table if exists
+DROP TABLE IF EXISTS votes;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS Stories;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS chapters;
+
+-- Drop table if exists
+DROP TABLE IF EXISTS users;
