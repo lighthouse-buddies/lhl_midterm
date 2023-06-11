f47c35a95c7210a1698c1abfe79da7ea367e9f28
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-11 03:08:00 -0400

    removed grant.sql

--- db/schema/05_grant.sql
+++
@@ -1,5 +0,0 @@
-
--- Grant all privileges on the "midterm" database to the user "labber"
-GRANT ALL PRIVILEGES ON DATABASE midterm TO labber;
--- Grant USAGE privilege on all sequences in the "public" schema to the user "labber"
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO labber;
