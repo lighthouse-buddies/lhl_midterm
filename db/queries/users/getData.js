5e726c92ac627b9af796a850e106e6a800fe5780
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-11 04:36:53 -0400

    using sequelize.js

--- db/queries/users/getData.js
+++ db/queries/users/getData.js
@@ -1,34 +1,25 @@
-const db = require('../../connection');
+const User = require('../../sequelize').User;
 
 /**
  * Retrieves a user by their user ID.
  * @param {number} user_id - The ID of the user.
  * @returns {Promise<Object|null>} A promise that resolves to an object containing user details if found, or null if not found.
  */
-const getData = (user_id) => {
-  // Prepare the SQL query with parameter placeholders
-  const query = 'SELECT * FROM users WHERE id = $1;';
+const getData = async (user_id) => {
+  try {
+    const user = await User.findByPk(user_id);
 
-  // Prepare the values to be inserted into the query
-  const values = [user_id];
-
-  // Execute the query using the database connection
-  return db.query(query, values)
-    .then(data => {
-      // Check if a matching user was found
-      if (data.rows.length === 1) {
-        // Extract the user details from the query response
-        const { id, username, email, password, deleted_at } = data.rows[0];
-        return { id, username, email, deleted_at };
-      } else {
-        return null; // User not found
-      }
-    })
-    .catch(error => {
-      console.error('Error retrieving user:', error);
-      return null; // Error occurred
-    });
+    if (user) {
+      // Extract the user details from the user object
+      const { id, username, email, deleted_at } = user;
+      return { id, username, email, deleted_at };
+    } else {
+      return null; // User not found
+    }
+  } catch (error) {
+    console.error('Error retrieving user:', error);
+    return null; // Error occurred
+  }
 };
 
-module.exports =
-  getData;
+module.exports = getData;
