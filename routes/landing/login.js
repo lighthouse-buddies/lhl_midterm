18338457734bd0439a5fd4491cdbd4c0b918cff4
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-11 07:11:34 -0400

    formatted + extra exception handling

--- routes/landing/login.js
+++ routes/landing/login.js
@@ -1,28 +1,30 @@
 const Users = require('../../db/queries/users/users');
 
 const loginGetHandler = (req, res) => {
-  if(req.session.userId){
+  if (req.session.userId) {
     return res.redirect('/');
   }
-  res.render('login', {userCookie: req.session.userId});
-}
+  res.render('login', { userCookie: req.session.userId });
+};
 
-const loginPostHandler = (req, res) => {
+const loginPostHandler = async (req, res) => {
   console.log(req.body);
-  Users.authenticate(req.body.email, req.body.password).then((userId) => {
-    if (userId !== null) {
-      req.session.userId = userId; //TODO: set user cookie
-      res.redirect('/');
+
+  try {
+    const userId = await Users.authenticate(req.body.email, req.body.password);
+    if (userId == null) {
+        res.send('Incorrect email or password');
     } else {
-      res.status(500).send('User Login failed');
+      req.session.userId = userId; // TODO: set user cookie
+      res.redirect('/');
     }
-  });
-
-
-
-}
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('User Login failed');
+  }
+};
 
 module.exports = {
   get: loginGetHandler,
-  post: loginPostHandler
+  post: loginPostHandler,
 };
