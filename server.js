3346ec8c0691e847ff9e716201c898989805cf5a
monki1 <114178697+monki1@users.noreply.github.com>
2023-06-11 07:09:52 -0400

    clean up, using landing router

--- server.js
+++ server.js
@@ -33,59 +33,34 @@ app.use(express.static('public'));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const userApiRoutes = require('./routes/users-api');
-const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users/users');
-const chapterRoutes = require('./routes/chapters/chapters-api');
-const storyRoutes = require('./routes/stories/stories-api');
+const chapterRoutes = require('./routes/chapters-api');
+const storyRoutes = require('./routes/stories-api');
 const voteRoutes = require('./routes/votes/votes-api');
+const landingRoutes = require('./routes/landing-api');
 
-const storiesIndexHandler = require('./routes/pages/storiesIndex');
-const loginHandler = require('./routes/pages/login');
-const registerHandler = require('./routes/pages/register');
-const logoutGetHandler = require('./routes/pages/logoutGet');
-// Mount all resource routes
-// Note: Feel free to replace the example routes below with your own
-// Note: Endpoints that return data (eg. JSON) usually start with `/api`
-app.use('/api/users', userApiRoutes);
-app.use('/api/widgets', widgetApiRoutes);
-app.use('/', usersRoutes);
+
+
+
+
+
+
+
+
+app.use('/', landingRoutes);
 app.use('/chapters', chapterRoutes);
 app.use('/stories', storyRoutes);
 app.use('/votes', voteRoutes);
+app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
-
+//
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', storiesIndexHandler);
-app.get('/login', loginHandler.get);
-app.post('/login', loginHandler.post);
-app.get('/register', registerHandler.get);
-app.post('/register', registerHandler.post);
-app.get('/logout', logoutGetHandler);
 
-//dummy routes to style
-app.get('/stories', (req, res) => {
-  res.render('stories_show');
-});
 
-app.get('/create', (req, res) => {
-  res.render('stories_create');
-});
 
-app.get('/login', (req, res) => {
-  res.render('login');
-});
-
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
-app.get('/mystories', (req, res) => {
-  res.render('my_stories');
-});
 
 //end of dummy routes
 
