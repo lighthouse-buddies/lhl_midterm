6e73f82d664b2698c6d78b1f1052fefb148ce1f0
Tamara Tell <2telltamara@gmail.com>
2023-06-11 22:00:12 -0400

    res.redirect added on routes/stories/completeStory and edit preview display chapter_show

--- routes/stories/query/completeStory.js
+++ routes/stories/query/completeStory.js
@@ -4,24 +4,25 @@ const Stories = require('../../../db/queries/stories/stories');
 
 function completeStoryHandlerGenerator(value) {
   return (
-  async function completeStoryPostHandler(req, res) {
-    const storyId = req.body.storyId;
-    const userId = req.session.userId;
-    const authorID = await Stories.author(storyId);
-    if (!userId || authorID !== userId) {
-      res.status(401).send('Unauthorized');
-      return;
-    }
+    async function completeStoryPostHandler(req, res) {
+      const chapterId = req.body.chapterId;
+      const storyId = req.body.storyId;
+      const userId = req.session.userId;
+      const authorID = await Stories.author(storyId);
+      if (!userId || authorID !== userId) {
+        res.status(401).send('Unauthorized');
+        return;
+      }
 
 
-    let success = null;
-    try {
-      success = await Stories.setCompletion(storyId, value)
-    } catch (err) {
-      console.log(err);
+      let success = null;
+      try {
+        success = await Stories.setCompletion(storyId, value);
+      } catch (err) {
+        console.log(err);
+      }
+      res.redirect(`/chapters/${chapterId}`);
     }
-    res.json(success);
-  }
   );
 }
 
@@ -34,4 +35,4 @@ function completeStoryHandlerGenerator(value) {
 module.exports = {
   complete: completeStoryHandlerGenerator(true),
   unComplete: completeStoryHandlerGenerator(false)
-}
+};
